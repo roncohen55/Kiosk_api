2d3b5a88b3ecca20acaed49d2256d728c50c0472
roncohen55 <cohen74981@gmail.com>
2022-03-09 14:14:45 +0200

    09032022-1

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -2,12 +2,82 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const bcryptjs = require('bcryptjs'); 
 
-router.get('/sayHello',(request,response)=>{
+//model
+const User = require('../models/user');
+
+router.post('/createAccount', async (request,response) =>{
+    //1-get user inputs
+   const {firstName ,lastName ,email ,password ,mobile } = request.body;
+    //2-check if user exsist
+    User.findOne({ email: email  })
+    .then(async account => {
+
+        if(account){
+            return response.status(200).json({
+                message: 'User is already exsist ,please try other email'
+            });
+        }
+        else{
+            //3-crypt password
+             const formatted_password = await bcryptjs.hash(password,10);
+            //4-generate paasscode
+             const passcode = generateRandomIntegerInRange(1000,9999);
+             //5-create user in mongodb
+             const _user = new User ({
+                _id:mongoose.Types.ObjectId(),
+                email:email,
+                password:formatted_password,
+                mobile:mobile,
+                firstName:firstName,
+                lastName:lastName,
+                passcode:passcode
+            })
+            _user.save()
+            .then(account_created =>{
+                return response.status(200).json({
+                    message:account_created
+                });
+            })
+
+        }
+
+    })
+    .catch(err => {
+        return response.status(500).json({
+            message: err
+        });
+    })
+   
+
+   
+    
+    //6-response
+});
+function generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+router.get('/sayHello',async (request,response)=>{
+    try {
+        const users = await User.find();
+        return response.status(200).json({
+            message:users
+        });
+    } catch (error) {
+        return response.status(500).json({
+            message:error
+        });
+    }
     return response.status(200).json({
         message:' Hello from kiosk api'
     });
 })
+
+
+
 
 
 
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('../controllers/accounts');
+const Schema = mongoose.Schema;
+
+const  userSchema = new Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    email: String,
+    createdAt: {type:Date,default:Date.now},
+    password: String,
+    mobile: String,
+    avatar: {type:String,default:'https://cdn.vox-cdn.com/thumbor/00awoM5IS2kFITs9546UyMSePBY=/0x0:2370x1574/1200x800/filters:focal(996x598:1374x976)/cdn.vox-cdn.com/uploads/chorus_image/image/69715362/Screen_Shot_2020_07_21_at_9.38.25_AM.0.png'},
+    firstName: String,
+    lastName: String,
+    passcode: Number,
+    isBusiness: {type:Boolean,default:false},
+    isApproved: {type:Boolean,default:false}, 
+    isLocked: {type:Boolean,default:false}
+});
+
+module.exports = mongoose.model('User',userSchema);
